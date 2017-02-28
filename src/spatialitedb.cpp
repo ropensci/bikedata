85ce6aeab45abcc8085550e707f74a7b49bd94b4
mpadge <mark.padgham@email.com>
2017-02-28 18:38:16 +0100

    add convert_datetime fn to src/spatialitedb.cpp, and add trip_id column

--- src/spatialitedb.cpp
+++ src/spatialitedb.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <cstring> // strtok
 #include <iostream>
 #include <sys/sendfile.h>
 #include <fcntl.h>
@@ -50,6 +51,31 @@ char *strtokm(char *str, const char *delim)
     return tok;
 }
 
+// Datetime strings for NYC change between 08/2014 and 09/2014 from
+// yyyy-mm-dd HH:MM:SS to m/d/yyyy HH:MM:SS. sqlite3 can't combine dates in
+// different formats, so this converts the latter to former formats.
+std::string convert_datetime (std::string str)
+{
+    if (size_t ipos = str.find ("/") != std::string::npos)
+    {
+        std::string mm = str.substr (0, ipos);
+        if (ipos == 1)
+            mm = std::string ("0") + mm;
+        str = str.substr (ipos + 1, str.length () - ipos - 1);
+        ipos = str.find ("/");
+        std::string dd = str.substr (0, ipos).c_str ();
+        if (ipos == 1)
+            dd = std::string ("0") + dd;
+        str = str.substr (ipos + 1, str.length () - ipos - 1);
+        ipos = str.find (" ");
+        std::string yy = str.substr (0, ipos);
+        str = str.substr (ipos + 1, str.length () - ipos - 1);
+        str = yy + "-" + mm + "-" + dd + " " + str;
+    }
+
+    return str;
+}
+
 //' import data using spatialite
 //'
 //' Extracts bike data for NYC citibike
@@ -79,6 +105,7 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
 
     std::string createqry = "CREATE TABLE trips ("
         "id serial primary key,"
+        "trip_id integer,"
         "trip_duration numeric,"
         "start_time timestamp without time zone,"
         "stop_time timestamp without time zone,"
@@ -105,24 +132,24 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
     char * tail = 0;
     std::map <std::string, std::string> stationqry;
 
-    sprintf(sqlqry, "INSERT INTO trips VALUES (NULL, @TD, @ST, @ET, @SSID, @ESID, @BID, @UT, @BY, @GE)");
+    sprintf(sqlqry, "INSERT INTO trips VALUES (NULL, @TI, @TD, @ST, @ET, @SSID, @ESID, @BID, @UT, @BY, @GE)");
 
     sqlite3_prepare_v2(dbcon, sqlqry, BUFFER_SIZE, &stmt, NULL);
 
     sqlite3_exec(dbcon, "BEGIN TRANSACTION", NULL, NULL, &zErrMsg);
 
-    int ntrips = 0;
+    int trip_id = 0;
     for(unsigned int filenum = 0; filenum < datafiles.length(); filenum++) {
 
         if (!quiet)
-            Rcpp::Rcout << "reading file " << filenum << "/" <<
+            Rcpp::Rcout << "reading file " << filenum + 1 << "/" <<
                 datafiles.size() << ": " <<
                 datafiles [filenum] << std::endl;
 
         pFile = fopen(datafiles[filenum], "r");
         char * junk = fgets(in_line, BUFFER_SIZE, pFile);
 
-        std::string tripqrystart = "INSERT INTO trips (trip_duration, start_time, stop_time, start_station_id, end_station_id, bike_id, user_type, birth_year, gender) VALUES ";
+        std::string tripqrystart = "INSERT INTO trips (trip_id, trip_duration, start_time, stop_time, start_station_id, end_station_id, bike_id, user_type, birth_year, gender) VALUES ";
         std::string tripqry = tripqrystart;
 
         std::string birthyear = "", gender = "", 
@@ -132,13 +159,19 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
 
         while (fgets (in_line, BUFFER_SIZE, pFile) != NULL) {
 
-            ntrips++;
+            sqlite3_bind_text(stmt, 1, std::to_string (trip_id).c_str(), -1, SQLITE_TRANSIENT); // Trip ID
+            trip_id++;
+
             std::string in_line2 = in_line;
             boost::replace_all(in_line2, "\\N","\"\"");
             strtokm(&in_line2[0u], "\""); //First double speech marks
-            sqlite3_bind_text(stmt, 1, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Trip duration
-            sqlite3_bind_text(stmt, 2, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Start time
-            sqlite3_bind_text(stmt, 3, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Stop time
+            sqlite3_bind_text(stmt, 2, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Trip duration
+
+            std::string tempstr = convert_datetime (strtokm(NULL, "\",\""));
+            sqlite3_bind_text(stmt, 3, tempstr.c_str(), -1, SQLITE_TRANSIENT); // Start time
+            //sqlite3_bind_text(stmt, 3, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Start time
+            //
+            sqlite3_bind_text(stmt, 4, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Stop time
             startstationid = strtokm(NULL, "\",\"");
             if (stationqry.count(startstationid) == 0) {
                 stationqry[startstationid] = "(" + startstationid + "," + 
@@ -151,7 +184,7 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
                 strtokm(NULL, "\",\"");
             }
 
-            sqlite3_bind_text(stmt, 4, startstationid.c_str(), -1, SQLITE_TRANSIENT); // Start station ID
+            sqlite3_bind_text(stmt, 5, startstationid.c_str(), -1, SQLITE_TRANSIENT); // Start station ID
 
             endstationid = strtokm(NULL, "\",\"");
             if (stationqry.count(endstationid) == 0) {
@@ -165,9 +198,9 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
                 strtokm(NULL, "\",\"");
             }
 
-            sqlite3_bind_text(stmt, 5, endstationid.c_str(), -1, SQLITE_TRANSIENT); // End station ID
-            sqlite3_bind_text(stmt, 6, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Bicycle ID
-            sqlite3_bind_text(stmt, 7, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // User type
+            sqlite3_bind_text(stmt, 6, endstationid.c_str(), -1, SQLITE_TRANSIENT); // End station ID
+            sqlite3_bind_text(stmt, 7, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // Bicycle ID
+            sqlite3_bind_text(stmt, 8, strtokm(NULL, "\",\""), -1, SQLITE_TRANSIENT); // User type
             birthyear = strtokm(NULL, "\",\"");
             gender = strtokm(NULL, "\",\"");
             if (birthyear.empty()) {
@@ -176,8 +209,8 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
             if (gender.empty()) {
                 gender = "NULL";
             }
-            sqlite3_bind_text(stmt, 8, birthyear.c_str(), -1, SQLITE_TRANSIENT); // Birth Year
-            sqlite3_bind_text(stmt, 9, gender.c_str(), -1, SQLITE_TRANSIENT); // Gender
+            sqlite3_bind_text(stmt, 9, birthyear.c_str(), -1, SQLITE_TRANSIENT); // Birth Year
+            sqlite3_bind_text(stmt, 10, gender.c_str(), -1, SQLITE_TRANSIENT); // Gender
 
             sqlite3_step(stmt);
 
@@ -206,5 +239,5 @@ int importDataToSpatialite(CharacterVector datafiles, const char* spdb, bool qui
     if (rc != SQLITE_OK)
         throw std::runtime_error ("Unable to close sqlite database");
 
-    return(ntrips);
+    return(trip_id);
 }
