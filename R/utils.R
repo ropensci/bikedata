b173ddb7d2b3a697decf6ff5b1b8df2e8555862b
mpadge <mark.padgham@email.com>
2017-04-06 11:59:30 +0200

    improve convert_city_names

--- R/utils.R
+++ R/utils.R
@@ -7,13 +7,30 @@
 #' @noRd
 convert_city_names <- function (city)
 {
+    city <- substring (gsub ('[[:punct:]]', '', tolower (city)), 1, 3)
+    indx_lo <- which (city %in% c ('lon', 'los'))
+    indx <- which (!seq (city) %in% indx_lo)
+    if (length (indx_lo) > 0)
+    {
+        city_lo <- city [indx_lo]
+        city <- city [indx]
+        city_lo [city_lo == 'lon'] <- 'lo'
+        city_lo [city_lo == 'los'] <- 'la'
+    }
+    city <- substring (city, 1, 2)
+
     city_names <- c ('ny', 'ne', 'ci', # nyc citibike
                      'bo', 'hu', # boston hubway
                      'ch', 'di', # chicago divvy bike
                      'wa', 'dc', 'ca', # washington dc capital bike share
-                     'la', 'lo', 'me') # los angeles metro
+                     'la', 'me') # los angeles metro
     city_code <- c ('ny', 'ny', 'ny', 'bo', 'bo', 'ch', 'ch', 
-                    'dc', 'dc', 'dc', 'la', 'la', 'la') 
-    city <- substring (gsub ('[[:punct:]]', '', tolower (city)), 1, 2)
-    city_code [pmatch (city, city_names)]
+                    'dc', 'dc', 'dc', 'la', 'la') 
+    city_code <- city_code [pmatch (city, city_names)]
+
+    city <- rep (NA, length (city))
+    city [indx_lo] <- city_lo
+    city [indx] <- city_code
+
+    return (city)
 }
