676ee6d8808d9d80e007f35bde238554f1eb229c
mpadge <mark.padgham@email.com>
2018-04-03 22:18:52 +0200

    add license info for :gemini:MN

--- R/zzz.R
+++ R/zzz.R
@@ -11,6 +11,8 @@
                    "  provided and owned by Motivate International Inc.\n",
                    "  see https://www.capitalbikeshare.com/data-license-agreement\n", #nolint
                    "  and https://www.divvybikes.com/data-license-agreement\n",
-                   "  and https://www.thehubway.com/data-license-agreement")
+                   "  and https://www.thehubway.com/data-license-agreement\n",
+                   "Nice Ride Minnesota license",
+                   "  https://www.niceridemn.org/data_license")
     packageStartupMessage (msg)
 }
