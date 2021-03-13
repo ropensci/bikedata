17956eaf0b0d60a95c7546c9187763686cf2e430
mpadge <mark.padgham@email.com>
2021-03-13 21:33:34 +0100

    lint bikedata-files

--- R/bikedata-files.R
+++ R/bikedata-files.R
@@ -12,23 +12,23 @@
 #' c ('ny', 'dc', 'bo'), respectively.
 #'
 #' @noRd
-get_aws_bike_files <- function (bucket)
-{
+get_aws_bike_files <- function (bucket) {
+
     host <- "https://s3.amazonaws.com"
     aws_url <- sprintf ("https://%s.s3.amazonaws.com", bucket)
 
-    doc <- httr::content (httr::GET (aws_url), encoding = 'UTF-8')
+    doc <- httr::content (httr::GET (aws_url), encoding = "UTF-8")
     nodes <- xml2::xml_children (doc)
     # NOTE: xml2::xml_find_all (doc, ".//Key") should work here but doesn't, so
     # this manually does what that would do
     files <- lapply (nodes, function (i)
-                     if (grepl ('zip|csv', i))
+                     if (grepl ("zip|csv", i))
                          strsplit (strsplit (as.character (i),
-                                 "<Key>") [[1]] [2], "</Key>") [[1]] [1] )
+                                 "<Key>") [[1]] [2], "</Key>") [[1]] [1])
     files <- unlist (files)
 
     # nyc citibike data has a redundamt file as first item
-    if (bucket == 'tripdata')
+    if (bucket == "tripdata")
         files <- files [2:length (files)]
 
     paste0 (host, "/", bucket, "/", files)
@@ -43,23 +43,23 @@ get_aws_bike_files <- function (bucket)
 #' @return List of URLs used to download data
 #'
 #' @noRd
-get_london_bike_files <- function ()
-{
+get_london_bike_files <- function () {
+
     # First get list of base file names from AWS:
     aws_url <- "https://s3-eu-west-1.amazonaws.com/cycling.data.tfl.gov.uk/"
-    doc <- httr::content (httr::GET (aws_url), encoding  =  'UTF-8')
+    doc <- httr::content (httr::GET (aws_url), encoding  =  "UTF-8")
     nodes <- xml2::xml_children (doc)
-    getflist <- function (nodes, type = 'zip')
-    {
+    getflist <- function (nodes, type = "zip") {
+
         f <- lapply (nodes, function (i) if (grepl (type, i))
                      strsplit (strsplit (as.character (i), "<Key>") [[1]] [2],
                                "</Key>") [[1]] [1])
         basename (unlist (f))
     }
-    flist_zip <- getflist (nodes, type = 'zip')
-    flist_zip <- flist_zip [which (grepl ('usage', flist_zip))]
-    flist_csv <- getflist (nodes, type = 'csv')
-    flist_xlsx <- getflist (nodes, type = 'xlsx')
+    flist_zip <- getflist (nodes, type = "zip")
+    flist_zip <- flist_zip [which (grepl ("usage", flist_zip))]
+    flist_csv <- getflist (nodes, type = "csv")
+    flist_xlsx <- getflist (nodes, type = "xlsx")
 
     # Then convert to tfl.gov.uk filenames
     addr_base <- "http://cycling.data.tfl.gov.uk/usage-stats/"
@@ -74,23 +74,23 @@ get_london_bike_files <- function ()
 #' systems (currently LA and Philly).
 #'
 #' @noRd
-get_nabsa_files <- function (city)
-{
-    if (city == 'ph')
+get_nabsa_files <- function (city) {
+
+    if (city == "ph")
         the_url <- "https://www.rideindego.com/about/data/"
-    else if (city == 'la')
+    else if (city == "la")
         the_url <- "https://bikeshare.metro.net/about/data/"
     else
-        stop ('nabsa cities must be ph or la')
+        stop ("nabsa cities must be ph or la")
 
-    doc <- httr::content (httr::GET (the_url), encoding = 'UTF-8',
-                          as = 'parsed')
+    doc <- httr::content (httr::GET (the_url), encoding = "UTF-8",
+                          as = "parsed")
     hrefs <- xml2::xml_attr (xml2::xml_find_all (doc, ".//a"), "href")
     hrefs <- hrefs [which (grepl ("\\.zip", hrefs) &
                            !grepl ("[Ss]tation", hrefs))]
 
-    if (city == 'la')
-    {
+    if (city == "la") {
+
         the_url_sh <- "https://bikeshare.metro.net/"
         hrefs <- as.character (vapply (hrefs, function (i)
                                        gsub ("../../", the_url_sh, i, fixed = TRUE),
@@ -108,11 +108,11 @@ get_nabsa_files <- function (city)
 #' @return List of URLs used to download data
 #'
 #' @noRd
-get_montreal_bike_files <- function ()
-{
+get_montreal_bike_files <- function () {
+
     host <- "https://montreal.bixi.com/en/open-data"
     . <- NULL # suppress R CMD check note #nolint
-    nodes <- httr::content (httr::GET (host), encoding = 'UTF-8') %>%
+    nodes <- httr::content (httr::GET (host), encoding = "UTF-8") %>%
         xml2::xml_find_all (".//div")
     nodes <- nodes [which (xml2::xml_attr (nodes, "class") ==
                            "container open-data-history")]
@@ -128,12 +128,12 @@ get_montreal_bike_files <- function ()
 #' @return List of URLs used to download data
 #'
 #' @noRd
-get_guadala_bike_files <- function ()
-{
+get_guadala_bike_files <- function () {
+
     host_base <- "https://www.mibici.net"
     host <- paste0 (host_base, "/en/open-data/")
     . <- NULL # suppress R CMD check note #nolint
-    nodes <- httr::content (httr::GET (host), encoding = 'UTF-8') %>%
+    nodes <- httr::content (httr::GET (host), encoding = "UTF-8") %>%
         xml2::xml_find_all (".//div")
     nodes <- nodes [which (xml2::xml_attr (nodes, "class") ==
                            "unit one-quarter")]
@@ -152,28 +152,28 @@ get_guadala_bike_files <- function ()
 #' @return List of URLs used to download data
 #'
 #' @noRd
-get_bike_files <- function (city)
-{
-    aws_cities <- c ('ny', 'dc', 'bo', 'sf', 'ch')
-    buckets <- c ('tripdata', 'capitalbikeshare-data',
-                  'hubway-data', 'fordgobike-data', 'divvy-data')
-    nabsa_cities <- c ('la', 'ph')
+get_bike_files <- function (city) {
 
-    if (city %in% aws_cities)
-    {
+    aws_cities <- c ("ny", "dc", "bo", "sf", "ch")
+    buckets <- c ("tripdata", "capitalbikeshare-data",
+                  "hubway-data", "fordgobike-data", "divvy-data")
+    nabsa_cities <- c ("la", "ph")
+
+    if (city %in% aws_cities) {
+
         bucket <- buckets [match (city, aws_cities)]
         files <- get_aws_bike_files (bucket)
     } else if (city %in% nabsa_cities)
         files <- get_nabsa_files (city = city)
-    else if (city == 'gu')
+    else if (city == "gu")
         files <- get_guadala_bike_files ()
-    else if (city == 'lo')
+    else if (city == "lo")
         files <- get_london_bike_files ()
-    else if (city == 'mn')
-        warning ('Data for the Nice Ride MN system must be downloaded ',
-                 'manually from\nhttps://www.niceridemn.com/system-data/, and ',
-                 'loaded using store_bikedata')
-    else if (city == 'mo')
+    else if (city == "mn")
+        warning ("Data for the Nice Ride MN system must be downloaded ",
+                 "manually from\nhttps://www.niceridemn.com/system-data/, and ",
+                 "loaded using store_bikedata")
+    else if (city == "mo")
         files <- get_montreal_bike_files ()
 
     return (files)
