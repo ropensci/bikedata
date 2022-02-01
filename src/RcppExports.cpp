b878d269d7a4a00284c65e64762690a96b5aa2c2
mpadge <mark.padgham@email.com>
2022-02-01 12:19:55 +0100

    update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcpp_import_stn_df
 int rcpp_import_stn_df(const char * bikedb, Rcpp::DataFrame stn_data, std::string city);
 RcppExport SEXP _bikedata_rcpp_import_stn_df(SEXP bikedbSEXP, SEXP stn_dataSEXP, SEXP citySEXP) {
