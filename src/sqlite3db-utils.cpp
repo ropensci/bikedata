84da72a144b1728f08c4e5d02b830ac66cc3e5d0
mpadge <mark.padgham@email.com>
2026-03-09 12:46:35 +0100

    supress warnings about sprintf deprecation to close #107

--- src/sqlite3db-utils.cpp
+++ src/sqlite3db-utils.cpp
@@ -24,7 +24,7 @@ int db_utils::get_max_trip_id (sqlite3 * dbcon)
 {
     sqlite3_stmt * stmt;
     char qry_id [BUFFER_SIZE] = "\0";
-    int rc = sprintf(qry_id, "SELECT MAX(id) FROM trips");
+    int rc = snprintf(qry_id, BUFFER_SIZE, "SELECT MAX(id) FROM trips");
     rc = sqlite3_prepare_v2(dbcon, qry_id, BUFFER_SIZE, &stmt, nullptr);
     rc = sqlite3_step (stmt);
     int max_trip_id = sqlite3_column_int (stmt, 0);
@@ -45,7 +45,7 @@ int db_utils::get_max_stn_id (sqlite3 * dbcon)
 {
     sqlite3_stmt * stmt;
     char qry_id [BUFFER_SIZE] = "\0";
-    sprintf(qry_id, "SELECT MAX(id) FROM stations");
+    snprintf(qry_id, BUFFER_SIZE, "SELECT MAX(id) FROM stations");
     int rc = sqlite3_prepare_v2(dbcon, qry_id, BUFFER_SIZE, &stmt, nullptr);
     rc = sqlite3_step (stmt);
     int max_stn_id = sqlite3_column_int (stmt, 0);
@@ -66,7 +66,7 @@ int db_utils::get_stn_table_size (sqlite3 * dbcon)
 {
     sqlite3_stmt * stmt;
     char qry_id [BUFFER_SIZE] = "\0";
-    sprintf(qry_id, "SELECT COUNT(*) FROM stations");
+    snprintf(qry_id, BUFFER_SIZE, "SELECT COUNT(*) FROM stations");
     int rc = sqlite3_prepare_v2(dbcon, qry_id, BUFFER_SIZE, &stmt, nullptr);
     rc = sqlite3_step (stmt);
     int num_stns = sqlite3_column_int (stmt, 0);
